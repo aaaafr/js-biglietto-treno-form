7643b7055ce1e99b9935f31be9573ff7841b89b9
aaaafr <anastasia.frassini@icloud.com>
2021-05-11 18:37:32 +0200

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,77 @@
-// chiedi il nome
-var nome = prompt("Come ti chiami?");
-console.log(nome);
 
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e 
-var chilometri = prompt("Quanti km vuoi percorrere?");
-console.log(chilometri);
-// l'età del passeggero.
-var eta = prompt ("Quanti anni hai?")
-console.log(eta);
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-var prezzo = 0.21 * chilometri;
-console.log(prezzo);
-// va applicato uno sconto del 20% per i minorenni
-var scontoMin = (prezzo * 20)/100;
+document.getElementById('generate_ticket_button').addEventListener('click', genterateTicket);
+function genterateTicket(e) {
+    e.preventDefault();
+    var nameInputEl = document.getElementById('name');
+    var kmInputEl = document.getElementById('km');
+    var ageGroupSelectEl = document.getElementById('age_group');
+    var ticketEl = document.querySelector('.ticket_container');
 
-// va applicato uno sconto del 40% per gli over 65.
-var scontoOver = (prezzo * 40)/100;
+    var fullPrice = kmInputEl.value * 0.21
+    var discountText = 'Prezzo Pieno';
+   
+    if(ageGroupSelectEl.value == 'minorenne') {
+        fullPrice -= fullPrice * 0.2;
+        discountText = 'Sconto Minorenne';
 
-var risultato;
-if( eta < 18) {
-    console.log(risultato = prezzo - scontoMin);
-} else if (eta > 65) {
-    console.log(risultato = prezzo - scontoOver);
-} else {
-    console.log(risultato = prezzo);
+    } else if (ageGroupSelectEl.value == 'over65') {
+        fullPrice -= fullPrice * 0.4;
+        discountText = 'Sconto Senior';
+
+    }
+
+    
+
+
+    ticketEl
+    .insertAdjacentHTML('beforeend', 
+    `
+        <div class="ticket">
+            <div class="w-25 bg_grey">
+                Nome Passeggero ${nameInputEl.value}
+            </div>
+            <div class="ticket__details w-75 d_flex">
+                <div>
+                    <h3>Offerta</h3>
+                    <span>${discountText}</span>
+                    
+                </div>
+                <div>
+                    <h3>Carrozza</h3> 
+                    <span>${randomNumber(1, 10)}</span> 
+                </div>
+                <div>
+                    <h3>Codice CP</h3> 
+                    <span>${randomNumber(90000, 100000)}</span>
+                </div>
+                <div>
+                    <h3>Costo Biglietto</h3>
+                    <span>${fullPrice.toFixed(2)} €</span>
+                </div>
+            </div>
+        </div>
+    
+    `);
 }
 
-var mex;
- if(eta < 18){
-     mex = "Sconto Minori";
-     console.log(mex);
-     } else if(eta > 65){
-     mex = "Sconto Over 65";
-     console.log(mex);
- }else {
-    mex = "Nessuno sconto";
-    console.log(mex);
- }
 
-var carrozza = Math.floor(Math.random()*8);
+document.getElementById('cancel_ticket_button').addEventListener('click', function () {  
+    document.getElementById('name').value = "";
+    document.getElementById('km').value = "";
+    document.getElementById('age_group').value = "";
 
-var min = 90000;
-var max = 100000;
-var codiceTreno = Math.floor((Math.random() * (max - min) + min));
-console.log(codiceTreno);
+})
 
 
 
 
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-// document.getElementById("spesa").innerHTML = "SPESA TOTALE " + (Math.round((risultato + Number.EPSILON)) * 100 / 100);
-document.getElementById("person").innerHTML = nome;
-document.getElementById("sconto").innerHTML = mex;
-document.getElementById("carrozza").innerHTML = carrozza;
-document.getElementById("cod_treno").innerHTML = codiceTreno;
-document.getElementById("spesa").innerHTML =  "€ " + risultato.toFixed(2);
+//  Genera un numero random compreso tra un minimo e un massimo.
+//  @param {number} min - numero minimo del range
+//  @param {number} max  - numero massimo del range
+// @returns {number} numero casuale
 
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * ( max - min )) + min
+}
 
 
